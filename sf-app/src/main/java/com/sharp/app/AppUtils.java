01f814a4a580a5b36bc6da6175074f842d2e8a92
肖鹏 <xiaopeng1@sf-express.com>
2017-02-10 17:25:22 +0800

    init project by idea

---
+++ sf-app/src/main/java/com/sharp/app/AppUtils.java
@@ -0,0 +1,12 @@
+package com.sharp.app;
+
+import com.sharp.User;
+
+/**
+ * Created by 584003 on 2017/2/10.
+ */
+public class AppUtils {
+    public static void main(String[] args){
+        User user = new User();
+    }
+}
